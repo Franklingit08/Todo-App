3186be8d0f3857c7ad43472ade5c4f36410aab2d
Franklin geo <you@example.com>
2025-06-10 22:47:28 +0530

    added created todo

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,44 @@
 import express from 'express'
 import connectDb from './config/datab.js'
+import Todos from './model/todoModel.js'
 
-const app=express()
+const app = express()
 
-let port=3000
+let port = 3000
 
 connectDb()
 
-app.get('/',(req,res)=>(res.send('hai worked')
-))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.listen(port, ()=> console.log('server started'))
+
+
+app.post('/create-todo', async (req, res) => {
+
+
+    try {
+
+        let { title, description } = req.body
+
+
+        let newTodo = await Todos.create({
+            title,
+            description
+        })
+
+        res.json(newTodo)
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+
+})
+
+
+
+app.listen(port, () => console.log('server started'))
 
 
 
